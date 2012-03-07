97d38d1704a3167d2f16ae88d3a8e5709559d5d9
Milton Thomas <milton@spoontacular.com>
2012-03-06 23:35:04 -0500

    'Fixed' /cr so it ignites self

--- src/com/spoontacular/creativecontrol/CreativeControl.java
+++ src/com/spoontacular/creativecontrol/CreativeControl.java
@@ -4,12 +4,11 @@ import java.awt.Event;
 import java.util.List;
 
 import org.bukkit.entity.*;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import com.pneumaticraft.commandhandler.multiverse.Command;
 
 public class CreativeControl extends JavaPlugin {
 	
@@ -25,14 +24,11 @@ public class CreativeControl extends JavaPlugin {
        
     }
 	
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-	    if(label.equalsIgnoreCase("cr")){
-	        Player s = (Player)sender;
-	        List<Player> players = s.getWorld().getPlayers();
-	        Player target = players.get(players.indexOf(args[0])); // Gets the player who was typed in the command. 
-	        // For instance, if the command was "/ignite notch", then the player would be just "notch". 
-	        // Note: The first argument starts with [0], not [1]. So arg[0] will get the player typed. 
-	        target.setFireTicks(10000);
+	    if(cmd.getName().equalsIgnoreCase("cr")){
+	        Player s = (Player)sender;  
+	        s.setFireTicks(10000);
 	        return true;
 	    }
 	    return false;
