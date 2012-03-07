41aa8cd0921cec16354f5c8481e3daf76bbe3e17
Milton Thomas <milton@spoontacular.com>
2012-03-06 23:09:27 -0500

    Trying to get /cr command to work

--- src/com/spoontacular/creativecontrol/CreativeControl.java
+++ src/com/spoontacular/creativecontrol/CreativeControl.java
@@ -1,16 +1,32 @@
 package com.spoontacular.creativecontrol;
 
+import java.awt.Event;
 import java.util.List;
 
 import org.bukkit.entity.*;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.pneumaticraft.commandhandler.multiverse.Command;
 
 public class CreativeControl extends JavaPlugin {
+	
+	@Override
+    //When the plugin is enabled this method is called.
+    public void onEnable() {
+		//Create the pluginmanage pm.
+		PluginManager pm = getServer().getPluginManager();
+		//Get the infomation from the yml file.
+		PluginDescriptionFile pdfFile = this.getDescription();
+		//Print that the plugin has been enabled!
+		System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+       
+    }
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-	    if(cmd.getCommandName().equalsIgnoreCase("cr")){
+	    if(label.equalsIgnoreCase("cr")){
 	        Player s = (Player)sender;
 	        List<Player> players = s.getWorld().getPlayers();
 	        Player target = players.get(players.indexOf(args[0])); // Gets the player who was typed in the command. 
