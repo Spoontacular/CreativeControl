ae61bc63fd1f97578861efbb786654993a4afa43
Milton Thomas <milton@spoontacular.com>
2012-03-06 22:21:29 -0500

    Added a demo /cr command

--- src/com/spoontacular/creativecontrol/CreativeControl.java
+++ src/com/spoontacular/creativecontrol/CreativeControl.java
@@ -1,7 +1,24 @@
 package com.spoontacular.creativecontrol;
 
+import java.util.List;
+
+import org.bukkit.entity.*;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class CreativeControl extends JavaPlugin {
+import com.pneumaticraft.commandhandler.multiverse.Command;
 
+public class CreativeControl extends JavaPlugin {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+	    if(cmd.getCommandName().equalsIgnoreCase("ignite")){
+	        Player s = (Player)sender;
+	        List<Player> players = s.getWorld().getPlayers();
+	        Player target = players.get(players.indexOf(args[0])); // Gets the player who was typed in the command. 
+	        // For instance, if the command was "/ignite notch", then the player would be just "notch". 
+	        // Note: The first argument starts with [0], not [1]. So arg[0] will get the player typed. 
+	        target.setFireTicks(10000);
+	        return true;
+	    }
+	    return false;
+	}
 }
